f9303dbcb7f8c76c5450de5492665bee08fc4985
Cabinet-Mohammedi <cabinet.mohammedi17@gmail.com>
2025-11-10 23:51:36 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,5 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, update } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-
-// Configuration
-const firebaseConfig = {
+// Configuration Firebase (compat version pour GitHub Pages)
+var firebaseConfig = {
   apiKey: "AIzaSyBIrVOglgZALaaK6IwPwqHMiynBGD4Z3JM",
   authDomain: "mohammedi-cabinet.firebaseapp.com",
   databaseURL: "https://mohammedi-cabinet-default-rtdb.firebaseio.com",
@@ -13,7 +9,3 @@ const firebaseConfig = {
   appId: "1:666383356275:web:09de11f9dfa2451d843506",
   measurementId: "G-VT06BFXNP1"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
